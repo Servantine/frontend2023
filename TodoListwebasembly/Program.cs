aba4118dd6e0b50eab94b3c049ba9b4723a17ecf
Servantine <74580428+Servantine@users.noreply.github.com>
2023-10-11 23:34:25 +0700

    Register

--- TodoListwebasembly/Program.cs
+++ TodoListwebasembly/Program.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
 using TodoListwebasembly;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
+builder.Services.AddScoped(sp => 
+    new HttpClient
+    {
+        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+    });
